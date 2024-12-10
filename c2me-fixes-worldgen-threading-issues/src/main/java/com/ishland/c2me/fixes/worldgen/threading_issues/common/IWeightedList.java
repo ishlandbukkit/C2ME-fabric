b76f15c02dc6e0470aeaea2fed2136a8072d6079
ishland <ishlandmc@yeah.net>
2024-12-10 22:23:57 +0800

    chore: remove irrelevant WeightedList fix
    Cherry-picked from da29f9e0ec4fabab3c02f0ebde75f45619f7b7ac

--- c2me-fixes-worldgen-threading-issues/src/main/java/com/ishland/c2me/fixes/worldgen/threading_issues/common/IWeightedList.java
+++
@@ -1,9 +0,0 @@
-package com.ishland.c2me.fixes.worldgen.threading_issues.common;
-
-import net.minecraft.util.collection.WeightedList;
-
-public interface IWeightedList<U> {
-
-    public WeightedList<U> shuffleVanilla();
-
-}
