ca05d57a908760a6e2b8e7ca2124fbfa688eb57d
ishland <ishlandmc@yeah.net>
2024-12-10 22:13:16 +0800

    fix: silence RandomizedIntBlockStateProvider
    Cherry-picked from eaed7666803a83f0c10721f8302f6959877e17c8

--- c2me-fixes-worldgen-threading-issues/src/main/java/com/ishland/c2me/fixes/worldgen/threading_issues/mixin/threading/MixinRandomizedIntBlockStateProvider.java
+++ c2me-fixes-worldgen-threading-issues/src/main/java/com/ishland/c2me/fixes/worldgen/threading_issues/mixin/threading/MixinRandomizedIntBlockStateProvider.java
@@ -1,25 +1,53 @@
 package com.ishland.c2me.fixes.worldgen.threading_issues.mixin.threading;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.state.property.IntProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.intprovider.IntProvider;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 import net.minecraft.world.gen.stateprovider.RandomizedIntBlockStateProvider;
 import org.jetbrains.annotations.Nullable;
-import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(RandomizedIntBlockStateProvider.class)
-public class MixinRandomizedIntBlockStateProvider {
+public abstract class MixinRandomizedIntBlockStateProvider {
 
     @Shadow @Nullable private IntProperty property;
 
-    @Redirect(method = "get", at = @At(value = "FIELD", target = "Lnet/minecraft/world/gen/stateprovider/RandomizedIntBlockStateProvider;property:Lnet/minecraft/state/property/IntProperty;", opcode = Opcodes.PUTFIELD))
-    private void redirectGetProperty(RandomizedIntBlockStateProvider randomizedIntBlockStateProvider, IntProperty value) {
-        if (this.property != null) System.err.println("Detected different property settings in RandomizedIntBlockStateProvider! Expected " + this.property + " but got " + value);
-        synchronized (this) {
-            this.property = value;
+    @Shadow @Final private BlockStateProvider source;
+
+    @Shadow
+    @Nullable
+    private static IntProperty getIntPropertyByName(BlockState state, String propertyName) {
+        throw new AbstractMethodError();
+    }
+
+    @Shadow @Final private String propertyName;
+
+    @Shadow @Final private IntProvider values;
+
+    /**
+     * @author ishland
+     * @reason ensure proper behavior
+     */
+    @Overwrite
+    public BlockState get(Random random, BlockPos pos) {
+        BlockState blockState = this.source.get(random, pos);
+        IntProperty propertyLocal = this.property; // used as cache only
+        if (propertyLocal == null || !blockState.contains(propertyLocal)) {
+            IntProperty intProperty = getIntPropertyByName(blockState, this.propertyName);
+            if (intProperty == null) {
+                return blockState;
+            }
+
+            this.property = propertyLocal = intProperty;
         }
+
+        return blockState.with(propertyLocal, Integer.valueOf(this.values.get(random)));
     }
 
 }
